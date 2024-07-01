aa62244f171fb9334c714cf88cc136a2522cb835
sfchishti <sfc2022july@gmail.com>
2024-07-01 15:06:53 +0200

    Update __init__.py

--- hfqa_tool/__init__.py
+++ hfqa_tool/__init__.py
@@ -1,4 +1,4 @@
-from hfqa_tool.Combined_score import quality_score
 from hfqa_tool.Vocabulary_check import check_vocabulary
+from hfqa_tool.Combined_score import quality_score
 
-__all__ = ["quality_score", "check_vocabulary"]
+__all__ = ["check_vocabulary", "quality_score"]
