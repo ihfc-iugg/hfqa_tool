e4f5e2bd63cc8090aa1a48dc6c3e2787e315b8da
Saman Firdaus Chishti <sfc2022july@gmail.com>
2024-09-05 15:53:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'numpy>=1.18.0',
         'pandas>=1.0.0',
         'openpyxl>=3.0.0',
-        # 'glob', 'os', 'datetime', 'warnings' and 'math' are part of the standard library
+        # 'glob', 'os', 'datetime', 'warnings', 're' and 'math' are part of the standard library
     ],
     author="Saman Firdaus Chishti", # "Sven Fuchs", "Ben Norden", "Florian Neumann", "Elif Balkan-Pazvantoğlu", "Samah Elbarbary"
     author_email="chishti@gfz-potsdam.de, sfc2022july@gmail.com, chishti@uni-potsdam.de",
